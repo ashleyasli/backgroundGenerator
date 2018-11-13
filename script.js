e443869d58d8f5832f7ad581ed4bd426d3024d01
ashleyasli <43186823+ashleyasli@users.noreply.github.com>
2018-11-13 14:00:39 -0500

    adding random generate button

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
+var color3 = document.querySelector(".color3");
+var random = document.querySelector(".random");
 var body = document.getElementById("gradient");
 
-function setGradient() {
+function setColor() {
 	body.style.background = 
 	"linear-gradient(to right, " 
 	+ color1.value 
 	+ ", " 
-	+ color2.value 
+	+ color2.value
+	+ ", " 
+	+ color3.value 
 	+ ")";
 
 	css.textContent = body.style.background + ";";
 }
 
-color1.addEventListener("input", setGradient);
+function randomgenerator(){
+	var letters = '0123456789ABCDEF';
+	var color = '#';
+	for (var i = 0; i < 6; i++) {
+		color += letters[Math.floor(Math.random() * 16)];
+	}
+	return color;
+}
 
-color2.addEventListener("input", setGradient);
+function get_number(){
+	color1.value = randomgenerator();
+	color2.value = randomgenerator();
+	color3.value = randomgenerator();
+	setColor();
+}
+
+/*color1.addEventListener("input", setColor);
+
+color2.addEventListener("input", setColor);*/
